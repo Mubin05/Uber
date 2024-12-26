e951b9c14d7505d177a648907d6a2e5ac264dd87
Md Mubin <md456211@gmail.com>
2024-12-26 20:35:25 +0530

    modified:   Frontend/package-lock.json 	modified:   Frontend/package.json 	modified:   Frontend/src/main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,20 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import UserContex from './context/UserContex.jsx'
-import CaptainContext from './context/CaptainContext.jsx'
+import { BrowserRouter } from 'react-router-dom';
+import UserContext from './context/UserContext.jsx';
+import CaptainContext from './context/CapatainContext.jsx';
 import SocketProvider from './context/SocketContext.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CaptainContext>
-      <UserContex>
+      <UserContext>
       <SocketProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </SocketProvider>
-      </UserContex>
+      </UserContext>
     </CaptainContext>
   </StrictMode>,
 )
